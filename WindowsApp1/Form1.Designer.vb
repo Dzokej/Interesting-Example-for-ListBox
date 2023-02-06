f320a77a89046348687cd470311971321253afc6
Itachi <Itachi@LAPTOP-9MV3EQH4>
2023-02-06 12:52:31 -0600

    Add project files.

---
+++ WindowsApp1/Form1.Designer.vb
@@ -0,0 +1,65 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lstFlavors = New System.Windows.Forms.ListBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'lstFlavors
+        '
+        Me.lstFlavors.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lstFlavors.FormattingEnabled = True
+        Me.lstFlavors.ItemHeight = 31
+        Me.lstFlavors.Items.AddRange(New Object() {"Hello", "Dude", "Blah"})
+        Me.lstFlavors.Location = New System.Drawing.Point(509, 76)
+        Me.lstFlavors.Name = "lstFlavors"
+        Me.lstFlavors.Size = New System.Drawing.Size(183, 252)
+        Me.lstFlavors.TabIndex = 0
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 19.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(103, 86)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(287, 213)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.lstFlavors)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents lstFlavors As ListBox
+    Friend WithEvents Button1 As Button
+End Class
---
+++ WindowsApp1/Form1.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form1
+    Private Sub lstFlavors_SelectedIndexChanged(sender As Object, e As EventArgs) Handles lstFlavors.SelectedIndexChanged
+        If lstFlavors.SelectedIndex = 0 Then
+            Button1.Text = lstFlavors.SelectedItem.ToString
+        ElseIf lstFlavors.SelectedIndex = 1 Then
+            Button1.Text = lstFlavors.SelectedItem.ToString
+        ElseIf lstFlavors.SelectedIndex = 2 Then
+            Button1.Text = lstFlavors.SelectedItem.ToString
+        End If
+    End Sub
+End Class
